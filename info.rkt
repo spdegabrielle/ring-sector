02ad4e17ab0d6b857c0290b3b4b9a5b533abc5dc
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2025-07-28 02:17:55 +0100

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "ring-sector")
-(define deps '("base" "draw-lib"))
+(define deps '("base" "draw-lib" "gui-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/ring-sector.scrbl" () (draw-library))))
 (define pkg-desc "Draw a sector of a ring")
